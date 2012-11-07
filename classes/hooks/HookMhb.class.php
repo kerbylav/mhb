cc43c5ffdee39e7a0baeb4ac96e2d39d1d01c899
kerby <mail@kerby.ru>
2012-11-07 21:58:59 +0400

    more changes

---
+++ classes/hooks/HookMhb.class.php
@@ -0,0 +1,30 @@
+<?php
+/*-------------------------------------------------------
+*
+*   Must Have Blogs.
+*   Copyright © 2012 Alexei Lukin
+*
+*--------------------------------------------------------
+*
+*   Official site: imthinker.ru/stickytopics2
+*   Contact e-mail: kerbylav@gmail.com
+*
+---------------------------------------------------------
+*/
+
+class PluginMhb_HookMhb extends Hook
+{
+
+    public function RegisterHook()
+    {
+        $this->AddHook('template_admin_action_item', 'AddAdminEditMenu');
+    }
+
+    public function AddAdminEditMenu($aParams)
+    {
+        $res=$this->Viewer_Fetch($this->PluginMhb_Main_GetTemplateFilePath(__CLASS__, 'admin_edit_menu.tpl'));
+        return $res;
+    }
+
+}
+?>
---
+++ templates/skin/default/admin_edit_menu.tpl
@@ -0,0 +1,1 @@
+ <li><a href="{router page="admin"}mhb/">{$aLang.plugin.mhb.admin_admin_menu}</a></li>
